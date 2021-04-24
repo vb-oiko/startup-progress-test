cda7fcebdb230f55d9ea2416e39e830d3612cf18
Vasyl <vasyl@wordproof.com>
2021-04-24 16:11:14 +0300

    fix: App files, add ProgressPanel component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ProgressPanel from "./components/ProgressPanel";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid place-items-center w-screen h-screen bg-gradient-to-r from-gray-200 to=gray-300">
+      <ProgressPanel />
     </div>
   );
 }
---
+++ src/components/ProgressPanel.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProgressPanel = () => {
+  return <div>My startup progress</div>;
+};
+
+export default ProgressPanel;
