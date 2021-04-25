54973d2ebd30c47527e1684c203cfb432699eb77
Vasyl <vasyl@wordproof.com>
2021-04-25 11:26:33 +0300

    add: progress step cpomponent

--- src/components/ProgressStage.tsx
+++ src/components/ProgressStage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { StartupStage } from "../services/ProgressService";
+import ProgressStep from "./ProgressStep";
 import TickIcon from "./TickIcon";
 
 const ProgressPanel = ({
@@ -10,12 +11,21 @@ const ProgressPanel = ({
   stage: StartupStage;
 }) => {
   return (
-    <li className="flex mt-5 mb-4">
-      <span className="text-white text-sm font-semibold bg-gray-900 w-7 h-7 rounded-full inline-flex items-center justify-center">
-        {index + 1}
-      </span>
-      <span className="text-xl font-bold ml-3 mr-auto">{stage.name}</span>
-      <TickIcon />
+    <li>
+      <div className="flex mt-5 mb-4">
+        <span className="text-white text-sm font-semibold bg-gray-900 w-7 h-7 rounded-full inline-flex items-center justify-center">
+          {index + 1}
+        </span>
+        <span className="text-xl font-bold ml-3 mr-auto">{stage.name}</span>
+        <TickIcon />
+      </div>
+      <ul className="space-y-2">
+        {stage.steps.map((step, index) => (
+          <li>
+            <ProgressStep index={index} step={step} />
+          </li>
+        ))}
+      </ul>
     </li>
   );
 };
--- src/components/ProgressStep.tsx
+++ src/components/ProgressStep.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { StartupStep } from "../services/ProgressService";
+import { stringToKebab } from "../utils";
 
-const ProgressStep = () => {
-  return <div>My startup progress</div>;
+const ProgressStep = ({
+  index,
+  step,
+}: {
+  index: number;
+  step: StartupStep;
+}) => {
+  const id = `checkbox-${stringToKebab(step.name)}`;
+
+  const [checked, setChecked] = useState(step.completed);
+
+  return (
+    <div className="relative flex items-start">
+      <div className="flex items-center h-5">
+        <input
+          id={id}
+          name={id}
+          type="checkbox"
+          checked={checked}
+          className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+          onChange={() => {
+            setChecked(!checked);
+          }}
+        />
+      </div>
+      <div className="ml-3 text-sm">
+        <label htmlFor={id} className="font-medium text-gray-700">
+          {step.name}
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default ProgressStep;
